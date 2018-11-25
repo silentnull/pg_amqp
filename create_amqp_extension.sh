e937a9f480cbd2b1267bc792d47e24ecd0a039bf
Pawel Kasperek <pawel.kasperek@edpauto.com>
2018-11-25 20:40:25 +0100

    Fix update extension amqp

--- create_amqp_extension.sh
+++ create_amqp_extension.sh
@@ -18,7 +18,7 @@ fi
 cwd=$(pwd)
 
 # Install pg_amqp extension  
-cd /usr/src/ && rm -f -r pg_amqp && git clone https://github.com/silentnull/pg_amqp.git
+# cd /usr/src/ && rm -f -r pg_amqp && git clone https://github.com/silentnull/pg_amqp.git
 cd /usr/src/pg_amqp && USE_PGXS=1 make -s check && USE_PGXS=1 make install
 
 cd ${cwd}
@@ -27,8 +27,7 @@ cd ${cwd}
 #service postgresql restart
 
 ### Init extensions to database template 4                                                                                                
-# sudo -u postgres -H -- psql -v ON_ERROR_STOP=1 --username "${PG_USER}" -d ${PG_DATABASE} <<-EOSQL
-# 	DROP EXTENSION IF EXISTS amqp;
-# 	CREATE EXTENSION IF NOT EXISTS amqp;
-# EOSQL
+sudo -u postgres -H -- psql -v ON_ERROR_STOP=1 --username "${PG_USER}" -d ${PG_DATABASE} <<-EOSQL
+	ALTER EXTENSION amqp UPDATE;;
+EOSQL
  
